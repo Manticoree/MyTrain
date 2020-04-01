93344d00b35e9bb37adb01b5daf8599c3f5a024c
Manticoree <Alex.Agafonov1@yandex.ru>
2020-04-01 16:17:33 +0300

    Initial commit

---
+++ app/src/main/java/com/body/mytrain/entities/TrainElement.java
@@ -0,0 +1,15 @@
+package com.body.mytrain.entities;
+
+import eu.davidea.viewholders.FlexibleViewHolder;
+
+public class TrainElement extends AbstractFlexibleItem<> {
+
+
+
+
+
+    public class MyViewHolder extends FlexibleViewHolder {
+
+
+    }
+}
