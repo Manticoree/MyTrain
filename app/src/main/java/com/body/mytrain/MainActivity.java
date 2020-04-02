ac489f93e27e55a3af14f59417547db2951c6c1c
Manticoree <alex.agafonov1@yandex.ru>
2020-04-02 15:02:04 +0300

    добавлен первоначальный recyclerView

--- app/src/main/java/com/body/mytrain/MainActivity.java
+++ app/src/main/java/com/body/mytrain/MainActivity.java
@@ -1,14 +1,65 @@
 package com.body.mytrain;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import eu.davidea.flexibleadapter.FlexibleAdapter;
+import eu.davidea.flexibleadapter.items.IFlexible;
 
 import android.os.Bundle;
 
+import com.body.mytrain.entities.TrainElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.rvSportLists)
+    RecyclerView rvListTrain;
+
+    private FlexibleAdapter<IFlexible> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        getFlexibleAdapter();
     }
+
+
+    private void getFlexibleAdapter() {
+        rvListTrain.setHasFixedSize(true);
+        RecyclerView.LayoutManager manager = new LinearLayoutManager(this,
+                GridLayoutManager.VERTICAL, false);
+        rvListTrain.setLayoutManager(manager);
+        List<IFlexible> myItems = getDatabaseList();
+
+        mAdapter = new FlexibleAdapter<>(myItems);
+        mAdapter.addListener(this);
+        rvListTrain.setAdapter(mAdapter);
+    }
+
+
+
+    public List<IFlexible> getDatabaseList() {
+        List<IFlexible> list = new ArrayList<>();
+        list.add(new TrainElement("1", "Тренировка в зале", R.drawable.woman_in_gym));
+        list.add(new TrainElement("2", "Тренировка на улице", R.drawable.train_in_street));
+        list.add(new TrainElement("1", "Тренировка в зале", R.mipmap.woman_train_in_gym));
+        list.add(new TrainElement("1", "Тренировка в зале", R.mipmap.woman_train_in_gym));
+        list.add(new TrainElement("1", "Тренировка в зале", R.mipmap.woman_train_in_gym));
+        list.add(new TrainElement("1", "Тренировка в зале", R.mipmap.woman_train_in_gym));
+        list.add(new TrainElement("1", "Тренировка в зале", R.mipmap.woman_train_in_gym));
+        list.add(new TrainElement("1", "Тренировка в зале", R.mipmap.woman_train_in_gym));
+        return list;
+    }
+
+
+
 }
--- app/src/main/java/com/body/mytrain/entities/TrainElement.java
+++ app/src/main/java/com/body/mytrain/entities/TrainElement.java
@@ -1,14 +1,83 @@
 package com.body.mytrain.entities;
 
+import android.net.Uri;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.body.mytrain.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import eu.davidea.flexibleadapter.FlexibleAdapter;
+import eu.davidea.flexibleadapter.items.AbstractFlexibleItem;
+import eu.davidea.flexibleadapter.items.IFlexible;
 import eu.davidea.viewholders.FlexibleViewHolder;
 
-public class TrainElement extends AbstractFlexibleItem<> {
+public class TrainElement extends AbstractFlexibleItem<TrainElement.MyViewHolder> {
+
+    private String id;
+    private String title;
+    private int uri;
+
+
+    public TrainElement(String id, String title, int uri) {
+        this.id = id;
+        this.title = title;
+        this.uri = uri;
+    }
+
+    @Override
+    public boolean equals(Object inObject) {
+        if(inObject instanceof TrainElement) {
+            TrainElement inItem = (TrainElement) inObject;
+            return this.id.equals(inItem.id);
+        }
+        return false;
+    }
+
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
 
 
 
+    @Override
+    public int getLayoutRes() {
+        return R.layout.train_item;
+    }
 
+    @Override
+    public MyViewHolder createViewHolder(View view, FlexibleAdapter<IFlexible> adapter) {
+        return new MyViewHolder(view, adapter);
+    }
+
+    @Override
+    public void bindViewHolder(FlexibleAdapter<IFlexible> adapter, MyViewHolder holder,
+                               int position,
+                               List<Object> payloads) {
+
+        holder.tvTitle.setText(title);
+        Picasso.get()
+                .load(uri)
+                .into(holder.ivTrain);
+
+
+    }
 
     public class MyViewHolder extends FlexibleViewHolder {
+        public TextView tvTitle;
+        public ImageView ivTrain;
+
+        public MyViewHolder(View view, FlexibleAdapter adapter) {
+
+            super(view, adapter);
+            ivTrain = view.findViewById(R.id.ivPhotoTrain);
+            tvTitle = view.findViewById(R.id.tvTitle);
+        }
 
 
     }
